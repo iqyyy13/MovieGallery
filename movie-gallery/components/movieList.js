07a17b0848e3aa9cb4e6e0d324d94bc0036d9d68
iqyyy13 <muhdiqshan@gmail.com>
2024-10-24 15:38:26 +0800

    added carousel for toprated and upcoming

--- movie-gallery/components/movieList.js
+++ movie-gallery/components/movieList.js
@@ -1,31 +1,73 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Dimensions,
+  ScrollView,
+  Image,
+} from 'react-native'
 import React from 'react'
-import { ScrollView } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import Carousel from 'react-native-reanimated-carousel'
 
-const MovieList = () => {
-    return (
-        <View style = {styles.container}>
-          <View style = {styles.row}>
-            <Text style = {{marginLeft: 20, fontSize: 20, color: 'white'}}>Upcoming</Text>
-            <TouchableOpacity>
-              <Text style = {{marginRight: 20, fontSize: 20, color: 'orange'}}>See All</Text>
-            </TouchableOpacity>
-          </View>
-          <ScrollView>
-            
-          </ScrollView>
-        </View>
-    )
+const { width, height } = Dimensions.get('window')
+
+const MovieList = ({ title, data }) => {
+  let movieName = 'Venom'
+  const navigation = useNavigation()
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <Text style={{ marginLeft: 20, fontSize: 20, color: 'white' }}>
+          {title}
+        </Text>
+        <TouchableOpacity>
+          <Text style={{ marginRight: 20, fontSize: 20, color: 'orange' }}>
+            See All
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+      >
+        {data.map((item, index) => {
+          return (
+            <TouchableWithoutFeedback
+              key={index}
+              onPress={() => navigation.navigate('Movie', item)}
+            >
+              <View style={{marginTop: 20}}>
+                <Image
+                  source={require('../assets/image.png')}
+                  style={{width: width*0.33, height: height*0.22, borderRadius: 20, marginLeft: 10}}
+                />
+                <Text style={{color: 'white', textAlign: 'center',}}>
+                  {
+                    movieName.length>14? movieName.slice(0,14)+'...':movieName
+                  }
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          )
+        })}
+      </ScrollView>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container:{
-    marginTop: 20,
-    flex:1,
+  container: {
+    flex: 1,
+    marginTop: 10,
   },
   row: {
-    flexDirection:'row',
+    flexDirection: 'row',
     justifyContent: 'space-between',
-  }
+  },
 })
 export default MovieList
--- movie-gallery/screens/HomeScreen.js
+++ movie-gallery/screens/HomeScreen.js
@@ -8,14 +8,18 @@ import TrendingMovies from '../components/trendingMovies'
 import MovieList from '../components/movieList'
 import Loading from '../components/loading'
 
-import { fetchTrendingMovies } from '../api/moviedb'
+import { fetchTrendingMovies, fetchUpcomingMovies } from '../api/moviedb'
 
 const HomeScreen = ({ navigation }) => {
   const [trending, setTrending] = useState([])
+  const [upcoming, setUpcoming] = useState([1,2,3])
+  const [topRated, setTopRated] = useState([1,2,3])
+
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     getTrendingMovies()
+    // getUpcomingMovies()
   }, [])
 
   const getTrendingMovies = async () => {
@@ -26,6 +30,15 @@ const HomeScreen = ({ navigation }) => {
     }
     setLoading(false)
   }
+
+  // const getUpcomingMovies = async () => {
+  //   const data = await fetchUpcomingMovies()
+  //   console.log('got upcoming movies: ', data)
+  //   if (data && data.results) {
+  //     setUpcoming(data.results)
+  //   }
+  //   setLoading(false)
+  // }
 
   return (
     <View style={styles.container}>
@@ -51,7 +64,9 @@ const HomeScreen = ({ navigation }) => {
           contentContainerStyle={{ paddingBottom: 10 }}
         >
           {trending.length > 0 && <TrendingMovies data={trending} />}
-          <MovieList />
+
+          <MovieList title="Upcoming" data={upcoming} />
+          <MovieList title="Top Rated" data={topRated} />
         </ScrollView>
       )}
     </View>
